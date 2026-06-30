282808e555853922c4055383493a2c4dfc66fa52
dino-the-programer <dinop0928@gmail.com>
2026-06-30 23:08:40 +0530

    version-test-CI/CD github api

--- app/api/routes/root.py
+++ app/api/routes/root.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-DATA: dict = {"message": "hello world from NodeCom controller 1", "version": "1.0.1"}
+DATA: dict = {"message": "hello world from NodeCom controller 1", "version": "1.0.2"}
 
 @router.get("/")
 def read_root():
